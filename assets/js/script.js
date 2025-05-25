94ef1d51b31dfc5bee0a174ff7ce2f6f9ea114e7
ryanvetoriano <vetorianosilva@gmail.com>
2025-05-25 13:13:48 -0300

    Fazendo o sistema de login e cadastro

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+function cadastrar(event) {
+  event.preventDefault();
+  const nome = document.getElementById("idNome").value;
+  const cpf = document.getElementById("idCpf").value;
+  const email = document.getElementById("idEmail").value;
+  const telefone = document.getElementById("idTelefone").value;
+  const idade = document.getElementById("idIdade").value;
+  const genero = document.getElementById("idGenero").value;
+  const senha = document.getElementById("idSenha").value;
+  const confsenha = document.getElementById("idConfSenha").value;
+
+  localStorage.setItem("nome", nome);
+  localStorage.setItem("cpf", cpf);
+  localStorage.setItem("email", email);
+  localStorage.setItem("telefone", telefone);
+  localStorage.setItem("idade", idade);
+  localStorage.setItem("genero", genero);
+  localStorage.setItem("senha", senha);
+  localStorage.setItem("confsenha", confsenha);
+
+  if(senha === confsenha){
+    alert("Cadastro realizado com sucesso!");
+    window.location.href = "../index.html";
+} else {
+    alert("Cadastro não realizado")
+}
+}
+  
+
+function logar(event) {
+  event.preventDefault();
+  const email = document.getElementById("idEmail").value;
+  const senha = document.getElementById("idSenha").value;
+
+  const emailSalvo = localStorage.getItem("email");
+  const senhaSalva = localStorage.getItem("senha");
+
+  if (email === emailSalvo && senha === senhaSalva) {
+    alert("Login bem-sucedido!");
+    window.location.href = "assets/inicio.html";
+  } else {
+    alert("Usuário ou senha incorretos!");
+  }
+}
+
+function recuperarSenha(event) {
+  event.preventDefault();
+  const usuario = document.getElementById("usuario").value;
+  const novaSenha = document.getElementById("novaSenha").value;
+
+  const usuarioSalvo = localStorage.getItem("usuario");
+
+  if (usuario === usuarioSalvo) {
+    localStorage.setItem("senha", novaSenha);
+    alert("Senha redefinida com sucesso!");
+    window.location.href = "index.html";
+  } else {
+    alert("Usuário não encontrado!");
+  }
+}
